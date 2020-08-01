f38439e1e797a6063bcba3d8f0c9f702a0d42666
Donny Roufs <iamchets@yahoo.com>
2020-08-02 00:07:30 +0200

    exports notfound

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,4 +2,6 @@ import * as view from "./view/index";
 
 const routes = [{ exact: true, path: "/", name: "home", component: view.Home }];
 
+export const NotFound = view.NotFound;
+
 export default routes;
