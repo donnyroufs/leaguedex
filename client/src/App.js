2c145e409e02dde9dd17e50fd26fba3ed121d3c4
Donny Roufs <iamchets@yahoo.com>
2020-08-01 23:49:10 +0200

    added key to routes map

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router";
 import Layout from "./components/layout/Layout";
 import routes from "./routes";
 
 const App = () => {
+  useEffect(() => {
+    fetch("/note")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <Layout>
       <Switch>
         {routes.map((route) => (
-          <Route exact path={route.path} component={route.component} />
+          <Route
+            exact
+            path={route.path}
+            component={route.component}
+            key={route.path}
+          />
         ))}
       </Switch>
     </Layout>
