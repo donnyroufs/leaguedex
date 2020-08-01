e1cf6768c4d9f79838a2e6e25173e61163437523
Donny Roufs <iamchets@yahoo.com>
2020-08-02 00:18:00 +0200

    added notes request for testing purposes

--- client/src/view/home/Home.jsx
+++ client/src/view/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
 import { Container } from "./Home.styles";
 
 const Home = () => {
+  const { data, loading } = useFetch("/note");
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Container>
       <h3>Home</h3>
+      {data.map(({ _id, body }) => (
+        <p key={_id}>{body}</p>
+      ))}
     </Container>
   );
 };
