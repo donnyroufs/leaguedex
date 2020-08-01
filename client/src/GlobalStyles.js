1c121a5edb332f6beef5fc35e41e1c8b0c48b1f2
Donny Roufs <iamchets@yahoo.com>
2020-08-02 00:07:12 +0200

    added flex center

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,5 +1,13 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 import { normalize } from "styled-normalize";
+
+export const utils = {
+  flexCenter: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
 
 export default createGlobalStyle`
     ${normalize}
