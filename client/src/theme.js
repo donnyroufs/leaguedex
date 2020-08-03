7f6d2d48971428914d4663f8fac5bc84a8159ac1
Donny Roufs <iamchets@yahoo.com>
2020-08-03 16:33:42 +0200

    global styling and theming

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,7 +3,8 @@ export default {
   secondary: "#B8D0EC",
   third: "#A8BFDA",
   border: "#536579",
-  secondaryButton: "6E7E92",
+  secondaryButton: "#6E7E92",
   background: "#23303F",
   header: "#33465B",
+  placeholderText: "#697E97",
 };
