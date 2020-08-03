57891c6207e1b1f909ac14d878c0e3cc1dd0a386
Donny Roufs <iamchets@yahoo.com>
2020-08-03 16:34:38 +0200

    add mobile menu

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "./Layout.styles";
 import Header from "../header/HeaderContainer";
+import Menu from "../menu/Menu";
 
 const Layout = ({ children }) => {
   return (
     <Container>
       <Header />
       {children}
+      <Menu />
     </Container>
   );
 };
