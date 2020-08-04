b87c24789c464f017d53c14dd909d921ca3c5e5b
Donny Roufs <iamchets@yahoo.com>
2020-08-04 22:56:38 +0200

    login modal, no validation yet.

---
+++ client/src/components/modal/LoginModal.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useRef } from "react";
+import Modal from "./Modal";
+import { Form, Input, Label, Group, Footer } from "../styles/Form";
+import { Button } from "../../GlobalStyles";
+
+import { useModal } from "../../hooks/useModal";
+import { useAuth } from "../../hooks/useAuth";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const LoginModal = () => {
+  const [values, setValues] = useState(initialValues);
+
+  const auth = useAuth();
+  const { setModal, isOpen } = useModal();
+  const innerRef = useRef();
+
+  const handleOnChange = (e) =>
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    auth.login(values);
+  };
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      setValues(initialValues);
+      setModal(null);
+    }
+    auth.isAuthenticated && setModal(null);
+  }, [auth.isAuthenticated, setModal]);
+  useEffect(() => innerRef.current && innerRef.current.focus(), [isOpen]);
+
+  return (
+    <Modal setModal={setModal} isOpen={isOpen("login")} title="login">
+      <Form>
+        <Group>
+          <Label>username</Label>
+          <Input
+            type="text"
+            name="username"
+            value={values.username}
+            placeholder="enter username"
+            ref={innerRef}
+            onChange={handleOnChange}
+          />
+        </Group>
+        <Group>
+          <Label>password</Label>
+          <Input
+            type="password"
+            name="password"
+            value={values.password}
+            placeholder="enter password"
+            onChange={handleOnChange}
+          />
+        </Group>
+        <Button form onClick={handleLogin}>
+          Login
+        </Button>
+        <Footer>
+          <Footer.Button first>Don't have an account?</Footer.Button>
+          <Footer.Button>Forgot password?</Footer.Button>
+        </Footer>
+      </Form>
+    </Modal>
+  );
+};
+
+export default LoginModal;
