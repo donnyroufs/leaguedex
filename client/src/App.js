bac36df5880e4ca207adb3ca09bf19746f9fc36d
Donny Roufs <iamchets@yahoo.com>
2020-08-04 22:55:39 +0200

    renders login modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import Layout from "./components/layout/Layout";
+import LoginModal from "./components/modal/LoginModal";
 import routes from "./routes";
 
 const App = () => {
   return (
     <Layout>
+      <LoginModal />
       <Switch>
         {routes.map((route) => (
           <Route
