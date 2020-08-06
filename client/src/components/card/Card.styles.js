b015ca1ea38f83e35c1b55758306ebebcedb79bb
Donny Roufs <iamchets@yahoo.com>
2020-08-06 12:20:25 +0200

    removed obsolete code

--- client/src/components/card/Card.styles.js
+++ client/src/components/card/Card.styles.js
@@ -42,14 +42,4 @@ Container.Image = styled.img`
   object-position: center right;
   width: 100%;
   height: 100%;
-  /* &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    z-index: 10;
-    background-color: rgba(74, 83, 95, 0.85);
-  } */
 `;
