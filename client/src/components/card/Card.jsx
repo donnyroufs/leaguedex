44235781e5e513ff9e9e7933022bb6c628df85c6
Donny Roufs <iamchets@yahoo.com>
2020-08-06 12:06:42 +0200

    Implemented locks based on auth, changed hover, and container height.

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { Container } from "./Card.styles";
+import { useAuth } from "../../hooks/useAuth";
+import { Container, Lock, Overlay } from "./Card.styles";
 
 const Card = ({ image, name }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return (
+      <Container to="/">
+        <Overlay>
+          <Lock />
+        </Overlay>
+        <Container.Image src={image} alt={name} />
+      </Container>
+    );
+  }
+
   return (
     <Container to={`/champion/` + name}>
       <Container.Image src={image} alt={name} />
--- client/src/components/card/Card.styles.js
+++ client/src/components/card/Card.styles.js
@@ -1,17 +1,38 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { FaLock } from "react-icons/fa";
+
+export const Overlay = styled.div`
+  background: rgba(74, 83, 95, 0.75);
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 15;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Lock = styled(FaLock)`
+  position: absolute;
+  color: #bababa;
+  height: 64px;
+  width: 64px;
+`;
 
 export const Container = styled(Link)`
+  position: relative;
   border: 1px solid ${(props) => props.theme.border};
   display: block;
   width: 100%;
-  height: 200px;
+  height: 225px;
   transition: all 0.3s ease-in-out;
   cursor: pointer;
 
   &:hover {
     transform: scale(1.1);
-    filter: saturate(125%);
   }
 `;
 
@@ -21,4 +42,14 @@ Container.Image = styled.img`
   object-position: center right;
   width: 100%;
   height: 100%;
+  /* &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 10;
+    background-color: rgba(74, 83, 95, 0.85);
+  } */
 `;
