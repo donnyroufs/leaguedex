bdf6d1042d79e0c0c3707e53351fb086b67cc897
Donny Roufs <iamchets@yahoo.com>
2020-08-07 11:43:20 +0200

    searchbar functionallity

--- client/src/view/home/Home.jsx
+++ client/src/view/home/Home.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import InfoWidget from "../../components/widget/InfoWidget";
 import SearchWidget from "../../components/widget/SearchWidget";
 import Card from "../../components/card/CardContainer";
 import { Container } from "./Home.styles";
 
 const Home = ({ status, data }) => {
+  const [value, setValue] = useState("");
+
   if (status.loading) {
     return <p>Loading...</p>;
   }
@@ -13,13 +15,13 @@ const Home = ({ status, data }) => {
     <Container>
       <Container.Widgets>
         <InfoWidget />
-        <SearchWidget />
+        <SearchWidget value={value} setValue={setValue} />
       </Container.Widgets>
       <Container secondary>
         {status === "success" &&
-          data.map((champion) => (
-            <Card key={champion.id} champion={champion} />
-          ))}
+          data
+            .filter((champ) => champ.name.toLowerCase().includes(value))
+            .map((champion) => <Card key={champion.id} champion={champion} />)}
       </Container>
     </Container>
   );
