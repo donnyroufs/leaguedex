948b0f532a08a25a4ada8ea4345772ba0849122e
Donny Roufs <iamchets@yahoo.com>
2020-08-07 11:43:27 +0200

    searchbar functionallity

--- client/src/components/widget/SearchWidget.jsx
+++ client/src/components/widget/SearchWidget.jsx
@@ -3,11 +3,15 @@ import Widget from "./Widget";
 import { Input, InputWrapper } from "./Widget.styles";
 import { FaSearch } from "react-icons/fa";
 
-const SearchWidget = () => {
+const SearchWidget = ({ value, setValue }) => {
   return (
     <Widget title="Find Matchup">
       <InputWrapper>
-        <Input type="text" placeholder="Champion name..." />
+        <Input
+          type="text"
+          placeholder="Champion name..."
+          onChange={(e) => setValue(e.target.value.toLowerCase())}
+        />
         <FaSearch className="searchbar-icon" />
       </InputWrapper>
     </Widget>
