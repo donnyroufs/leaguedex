90105757454392b097223d8d9df9530544ea7d79
Donny Roufs <iamchets@yahoo.com>
2020-08-08 02:17:22 +0200

    added protected key to routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,27 @@
 import * as view from "./view/index";
 
 const routes = [
-  { exact: true, path: "/", name: "home", component: view.Home },
-  { exact: false, path: "*", name: "", component: view.NotFound },
+  {
+    exact: true,
+    path: "/",
+    name: "home",
+    component: view.Home,
+    protected: false,
+  },
+  {
+    exact: true,
+    path: "/admin/dashboard",
+    name: "dashboard",
+    component: view.Dashboard,
+    protected: true,
+  },
+  {
+    exact: false,
+    path: "*",
+    name: "",
+    component: view.NotFound,
+    protected: false,
+  },
 ];
 
 export default routes;
