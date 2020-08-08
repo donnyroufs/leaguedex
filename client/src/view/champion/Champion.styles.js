f767ebd0920d94ac5464bb7c695dc8d8d3d68ff7
Donny Roufs <iamchets@yahoo.com>
2020-08-09 00:00:27 +0200

    cleaned up champions page

--- client/src/view/champion/Champion.styles.js
+++ client/src/view/champion/Champion.styles.js
@@ -5,8 +5,9 @@ export const Container = styled.section`
   flex-flow: column nowrap;
   margin: 0 auto;
   max-width: 1400px;
+  max-height: 350px;
 
-  @media screen and (min-width: 968px) {
+  @media screen and (min-width: 1200px) {
     margin-top: 3rem;
     flex-flow: row nowrap;
   }
@@ -14,8 +15,10 @@ export const Container = styled.section`
 
 Container.Image = styled.img`
   object-fit: cover;
+  border-bottom: 1px solid ${(props) => props.theme.border};
 
-  @media screen and (min-width: 968px) {
+  @media screen and (min-width: 1200px) {
+    border: 1px solid ${(props) => props.theme.border};
     margin-left: 3rem;
     height: 350px;
     width: 500px;
@@ -23,6 +26,17 @@ Container.Image = styled.img`
 `;
 Container.Info = styled.div`
   margin: 3.5rem 3rem 3rem 3rem;
+
+  @media screen and (min-width: 1200px) {
+    margin: 0 3rem;
+    background: ${(props) => props.theme.header};
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    padding: 3rem;
+    border: 1px solid ${(props) => props.theme.border};
+  }
 `;
 
 Container.Tags = styled.div`
@@ -45,94 +59,3 @@ export const Tag = styled.div`
   border-radius: 3px;
   margin-right: 1rem;
 `;
-
-// export const Container = styled.section`
-//     display: flex;
-//     flex-flow: column nowrap;
-//     justify-content: space-between;
-
-//     width: 100%;
-//     height: 100%;
-//     max-width: 1400px;
-
-//     margin: 0 auto;
-
-//     line-height: ${(props) => props.theme.lineHeight};
-
-//     @media screen and (min-width: 968px) {
-//         padding: 50px;
-//     }
-// `;
-
-// export const ChampionInfo = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: space-between;
-
-//     @media screen and (min-width: 968px) {
-//         flex-direction: row;
-//         flex-wrap: nowrap;
-
-//         --gap: 40px;
-//         margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
-//         width: calc(100% + var(--gap));
-
-//         > * {
-//             margin: var(--gap) 0 var(--gap) var(--gap);
-//         }
-//     }
-
-//     .description {
-//         display: flex;
-//         flex-direction: column;
-//         justify-content: space-between;
-
-//         padding: 10px;
-
-//         @media screen and (min-width: 968px) {
-//             background-color: ${(props) => props.theme.header};
-//             border: 1px solid ${(props) => props.theme.border};
-//         }
-
-//         > p {
-//             align-self: flex-start;
-//             margin: 10px;
-
-//             :first-child {
-//                 color: ${(props) => props.theme.primary};
-//                 font-weight: bold;
-//                 text-transform: uppercase;
-//             }
-//         }
-//     }
-
-//     img {
-//         display: block;
-//         object-fit: cover;
-
-//         max-width: 100%;
-
-//         @media screen and (min-width: 968px) {
-//             border: 1px solid ${(props) => props.theme.border};
-//         }
-//     }
-// `;
-
-// export const Matchups = styled.div`
-//     width: 100%;
-//     height: 100%;
-
-//     padding: 10px;
-
-//     @media screen and (min-width: 968px) {
-//         background-color: ${(props) => props.theme.header};
-//         border: 1px solid ${(props) => props.theme.border};
-//     }
-
-//     .row {
-//         min-height: 50px;
-//         margin: 10px;
-
-//         background-color: ${(props) => props.theme.third};
-//     }
-// `;
