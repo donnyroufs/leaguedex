597a7761a1756df682c4370cc790ce3931d33007
Donny Roufs <iamchets@yahoo.com>
2020-08-08 02:27:08 +0200

    removed unused import

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useAuth } from "./hooks/useAuth";
 
 const ProtectedRoute = ({ component: Component, isAdmin, ...rest }) => {
   return (
