dec2cec7e4fc74af9e9c45247070b0dc78767b88
Robert Bijl <robertbijl@live.nl>
2020-08-12 12:52:14 +0200

    Added column filtering and did some basic styling

--- client/src/view/dashboard/Dashboard.styles.js
+++ client/src/view/dashboard/Dashboard.styles.js
@@ -1,28 +1,68 @@
 import styled from "styled-components";
 
 export const Styles = styled.div`
+  display: flex;
+  justify-content: center;
+  
   width: 80%;
   margin: 4rem auto;
   max-width: 1400px;
+
   table {
     border-spacing: 0;
     border: 1px solid ${(props) => props.theme.border};
-    tr {
+
+    th, td {
+      margin: 0;
+      padding: 1rem;
+      border-bottom: 1px solid ${(props) => props.theme.border};
+      
+      :last-child {
+        border-right: 0;
+      }
+    }
+
+    th {
+      border-right: 1px solid ${(props) => props.theme.border};
+    } 
+
+    tbody { 
+      tr {
+        :nth-child(odd) {
+          background-color: #f2f2f21c;
+        }
+      }
+
       :last-child {
         td {
           border-bottom: 0;
         }
       }
     }
-    th,
-    td {
-      margin: 0;
-      padding: 1rem;
-      border-bottom: 1px solid ${(props) => props.theme.border};
-      border-right: 1px solid ${(props) => props.theme.border};
-      :last-child {
-        border-right: 0;
-      }
+  }
+
+  input {
+    border: 1px solid ${(props) => props.theme.border};
+    background: transparent;
+    color: ${(props) => props.theme.third};
+    width: 100%;
+    min-width: 150px;
+
+    ::placeholder {
+      color: ${(props) => props.theme.placeholderText};
+      opacity: 1;
+    }
+
+    :-ms-input-placeholder {
+      color: ${(props) => props.theme.placeholderText};
+    }
+
+    ::-ms-input-placeholder {
+      color: ${(props) => props.theme.placeholderText};
+    }
+
+    @media screen and (min-width: 968px) {
+      padding: 0.7rem 0.8rem;
     }
   }
 `;
