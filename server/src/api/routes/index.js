a72d504132b33bb280ec31583186b29f00352915
Donny Roufs <iamchets@yahoo.com>
2020-08-15 13:49:44 +0200

    routes for matchups

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const userRoutes = require('./user.routes');
 const championRoutes = require('./champion.routes');
+const matchupRoutes = require('./matchup.routes');
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/champion', championRoutes);
+router.use('/matchup', matchupRoutes);
 
 module.exports = router;
