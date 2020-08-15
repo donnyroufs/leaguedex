2ba4fe76cd303d8e58f06771bbea023b7e1941c2
Donny Roufs <iamchets@yahoo.com>
2020-08-15 13:49:33 +0200

    auth middleware added

--- server/src/api/routes/champion.routes.js
+++ server/src/api/routes/champion.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Controller = require('../controllers/Controller');
 const { db } = require('../../config/database');
+const Auth = require('../../lib/Auth');
 
 const router = express.Router();
 const controller = new Controller(db.champion);
