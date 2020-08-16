3f1aca8746be3b4a9ec742df187343c800f62b13
Donny Roufs <iamchets@yahoo.com>
2020-08-16 12:21:31 +0200

    get a list of public endpoints when going to api

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -6,6 +6,15 @@ const matchupRoutes = require('./matchup.routes');
 
 const router = express.Router();
 
+router.use('/', (req, res) => {
+  res.json({
+    public_endpoints: {
+      '/champion': 'get all league of legends champions',
+      '/champion/:name': 'Get a champion by name',
+    },
+  });
+});
+
 router.use('/user', userRoutes);
 router.use('/champion', championRoutes);
 router.use('/matchup', matchupRoutes);
