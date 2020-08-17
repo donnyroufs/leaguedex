e3c4c7481fd34c39bb8740a817a67e89eda53698
Donny Roufs <iamchets@yahoo.com>
2020-08-17 19:47:20 +0200

    only page load check if in a game

--- client/src/components/header/HeaderContainer.jsx
+++ client/src/components/header/HeaderContainer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useMatch } from "../../hooks/useMatch";
 import Header from "./Header";
+import { useAuth } from "../../hooks/useAuth";
 
 const HeaderContainer = () => {
+  const { isAuthenticated } = useAuth();
+  const { hasMatch, findMatch } = useMatch();
+
+  useEffect(() => {
+    if (!hasMatch && isAuthenticated) {
+      findMatch();
+    }
+  }, [hasMatch, isAuthenticated]);
+
   return <Header />;
 };
 
