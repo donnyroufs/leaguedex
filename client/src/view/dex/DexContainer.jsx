c27c67827fe962343e1a6eb9878d59f269e952e4
Donny Roufs <iamchets@yahoo.com>
2020-08-18 19:42:39 +0200

    quick and ugly implementation of creating notes.

--- client/src/view/dex/DexContainer.jsx
+++ client/src/view/dex/DexContainer.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 import Dex from "./Dex";
 import { getToken } from "../../helpers/getToken";
 import { useMatch } from "../../hooks/useMatch";
+
+const fetchNotes = async (id) => {
+  const res = await fetch(`/api/note/dex/${id}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: getToken(),
+    },
+    credentials: "include",
+  });
+  return res.json();
+};
 
 const fetchLatest = async (id) => {
   const res = await fetch(`/api/matchup/latest/${id}`, {
@@ -15,13 +28,42 @@ const fetchLatest = async (id) => {
   return res.json();
 };
 
+const fetchCreateNote = async (payload) => {
+  const res = await fetch(`/api/note/create`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: getToken(),
+    },
+    credentials: "include",
+  });
+
+  return res.json();
+};
+
 const DexContainer = ({ history }) => {
+  const { id } = useParams();
   const { setMatch, match } = useMatch();
+  const [notes, setNotes] = useState([]);
+
+  const createNote = async (e, noteValue) => {
+    e.preventDefault();
+
+    const data = await fetchCreateNote({
+      content: noteValue,
+      matchupId: id,
+      tags: null,
+    });
+
+    setNotes((current) => [...current, data]);
+  };
 
   const finishMatch = (e) => {
     e.preventDefault();
     (async () => {
-      if (match.gameId) {
+      if (match && match.gameId) {
         try {
           const data = await fetchLatest(match.gameId);
           if (data.updated) {
@@ -31,11 +73,35 @@ const DexContainer = ({ history }) => {
         } catch (err) {
           return null;
         }
+      } else {
+        history.push("/");
       }
     })();
   };
 
-  return <Dex finishMatch={finishMatch} />;
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await fetchNotes(id);
+        setNotes(data);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+
+    return () => {
+      setNotes([]);
+    };
+  }, [id]);
+
+  return (
+    <Dex
+      finishMatch={finishMatch}
+      createNote={createNote}
+      notes={notes}
+      history={history}
+    />
+  );
 };
 
 export default DexContainer;
