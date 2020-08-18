42e3d9510ec95c8ca6bf5ab9475d8023b3cc7113
Donny Roufs <iamchets@yahoo.com>
2020-08-18 13:35:19 +0200

    added note routes

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const userRoutes = require('./user.routes');
 const championRoutes = require('./champion.routes');
 const matchupRoutes = require('./matchup.routes');
+const noteRoutes = require('./note.routes');
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/champion', championRoutes);
 router.use('/matchup', matchupRoutes);
+router.use('/note', noteRoutes);
 router.use('/', (req, res) => {
   res.json({
     public_endpoints: {
