6bc9aab665a52fbb398f628fbe46f2f4827c5193
Donny Roufs <iamchets@yahoo.com>
2020-08-18 18:25:00 +0200

    checks if the game is done

--- client/src/view/dex/DexContainer.jsx
+++ client/src/view/dex/DexContainer.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { useParams } from "react-router";
 import Dex from "./Dex";
+import { getToken } from "../../helpers/getToken";
+import { useMatch } from "../../hooks/useMatch";
 
-const DexContainer = () => {
-  return <Dex />;
+const fetchLatest = async (id) => {
+  const res = await fetch(`/api/matchup/latest/${id}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: getToken(),
+    },
+    credentials: "include",
+  });
+  return res.json();
+};
+
+const DexContainer = ({ history }) => {
+  const { setMatch, match } = useMatch();
+
+  const finishMatch = (e) => {
+    e.preventDefault();
+    (async () => {
+      if (match.gameId) {
+        try {
+          const data = await fetchLatest(match.gameId);
+          if (data.updated) {
+            history.push("/");
+            setMatch(null);
+          }
+        } catch (err) {
+          return null;
+        }
+      }
+    })();
+  };
+
+  return <Dex finishMatch={finishMatch} />;
 };
 
 export default DexContainer;
