a00d2aa5d835c857a43dabff6092b8a38a559972
Donny Roufs <iamchets@yahoo.com>
2020-08-22 00:18:52 +0200

    refactor finding game

--- client/src/components/header/HeaderContainer.jsx
+++ client/src/components/header/HeaderContainer.jsx
@@ -1,19 +1,7 @@
-import React, { useEffect } from "react";
-import { useMatch } from "../../hooks/useMatch";
+import React from "react";
 import Header from "./Header";
-import { useAuth } from "../../hooks/useAuth";
 
 const HeaderContainer = () => {
-  const { isAuthenticated } = useAuth();
-  const { hasMatch, findMatch } = useMatch();
-
-  useEffect(() => {
-    if (!hasMatch && isAuthenticated) {
-      findMatch();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasMatch, isAuthenticated]);
-
   return <Header />;
 };
 
