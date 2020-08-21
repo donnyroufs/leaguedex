d8417837cfedf63f33b6b8aa8d5ab7a1896733b6
Donny Roufs <iamchets@yahoo.com>
2020-08-22 00:33:08 +0200

    only render menu on mobile

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Container, Wrapper } from "./Layout.styles";
 import Header from "../header/HeaderContainer";
 import Menu from "../menu/Menu";
+import { isMobile } from "react-device-detect";
 
 const Layout = ({ children }) => {
   return (
     <Container>
       <Header />
       <Wrapper>{children}</Wrapper>
-      <Menu />
+      {isMobile && <Menu />}
     </Container>
   );
 };
