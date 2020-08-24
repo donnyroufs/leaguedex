385ca4c42ba5b2dfcdf4125b60f1f6168aeff685
Donny Roufs <iamchets@yahoo.com>
2020-08-24 16:03:55 +0200

    only show loader on the actual content

--- client/src/components/styles/Loader.js
+++ client/src/components/styles/Loader.js
@@ -18,8 +18,6 @@ export const Container = styled.div`
   ${(props) =>
     props.secondary &&
     css`
-      top: 100px;
-      height: calc(100vh - 100px);
       background: transparent;
       z-index: 8000;
     `}
