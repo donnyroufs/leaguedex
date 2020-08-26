6ada92ae34bf88515d4856d7ba9b806a55eee350
Donny Roufs <iamchets@yahoo.com>
2020-08-26 13:41:44 +0200

    unused var

--- server/src/api/routes/champion.routes.js
+++ server/src/api/routes/champion.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Controller = require('../controllers/Controller');
 const { db } = require('../../config/database');
-const Auth = require('../../lib/Auth');
 
 const router = express.Router();
 const controller = new Controller(db.champion);
