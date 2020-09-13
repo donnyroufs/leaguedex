b5f1fcf95ff8c8dd3f56d4cc803645146b17ab42
Donny Roufs <iamchets@yahoo.com>
2020-09-13 13:37:48 +0200

    fix for staging?

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv/config');
+// require('dotenv').config({ path: '../.env' });
 
 const path = require('path');
 const express = require('express');
