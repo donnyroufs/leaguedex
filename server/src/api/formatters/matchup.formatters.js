39b3fa8f7ffe5d5e13730709240d785f605b0b43
Donny Roufs <iamchets@yahoo.com>
2020-10-03 11:36:38 +0200

    json formatter for champion page

--- server/src/api/formatters/matchup.formatters.js
+++ server/src/api/formatters/matchup.formatters.js
@@ -3,4 +3,11 @@ module.exports = {
     count: props.count,
     gamesPlayed: props.data.reduce((acc, curr) => acc + curr.games_played, 0),
   }),
+  getPlayedMatchups: (matchups) =>
+    matchups.map((props) => ({
+      ...props,
+      win_ratio: ((props.games_won / props.games_played) * 100).toFixed() + '%',
+      opponent: props.championB.name,
+      private: props.private ? 'private' : 'public',
+    })),
 };
