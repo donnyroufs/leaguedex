4e5e55706aa0a5a7b275574123bdb6e9f1cdc94c
Donny Roufs <iamchets@yahoo.com>
2020-10-04 10:06:46 +0200

    basic table styling

--- client/src/view/champion/Champion.styles.js
+++ client/src/view/champion/Champion.styles.js
@@ -34,7 +34,51 @@ Container.Header.Left = styled.div`
   }
 `;
 
-Container.Body = styled.main``;
+Container.Body = styled.main`
+  overflow-x: auto;
+  border-radius: 6px;
+  min-height: 200px;
+  table {
+    border-collapse: collapse;
+    min-width: 600px;
+    width: 100%;
+    color: ${(props) => props.theme.secondary};
+  }
+
+  thead {
+    color: #fff;
+    border-bottom: 20px solid ${(props) => props.theme.background};
+  }
+
+  thead > tr {
+    height: 80px;
+    background: ${(props) => props.theme.primary};
+  }
+
+  tbody > tr {
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+
+  /* tbody > tr:nth-child(even) {
+    background: #293748;
+  } */
+
+  tbody > tr:hover {
+    /* background: #3a4d65; */
+    background: #293748;
+  }
+
+  tr {
+    height: 64px;
+    border-bottom: 3px solid ${(props) => props.theme.background};
+    background: #232f3e;
+  }
+
+  td {
+    text-align: center;
+  }
+`;
 
 export const Title = styled.h3`
   grid-area: title;
