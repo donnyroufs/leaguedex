d4723e1ed0556b703a5edb440b8300662038b0be
Donny Roufs <iamchets@yahoo.com>
2020-10-11 13:49:18 +0200

    add formatters

--- server/src/api/routes/note.routes.js
+++ server/src/api/routes/note.routes.js
@@ -5,16 +5,17 @@ const { createValidator } = require('express-joi-validation');
 const { createNote } = require('../validators/Note.validators');
 const wrap = require('../../helpers/wrap');
 const model = require('../models/Note.model');
+const formatters = require('../formatters/note.formatters');
 
 const validator = createValidator();
 
 const router = express.Router();
-const controller = new Controller(model);
+const controller = new Controller(model, formatters);
 
 router.use(Auth.authenticateToken);
 
+router.get('/dex/:id', wrap(controller.findByMatchId));
 router.post('/create', validator.body(createNote), wrap(controller.createOne));
 router.delete('/:noteId', wrap(controller.deleteOne));
-router.get('/dex/:id', wrap(controller.findByMatchupId));
 
 module.exports = router;
