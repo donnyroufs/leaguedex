ac10bc257e9c68794ad42937a443046d5b5467ac
Donny Roufs <iamchets@yahoo.com>
2020-10-11 12:46:37 +0200

    wrap is not a fn fix, add model to controller

--- server/src/api/routes/note.routes.js
+++ server/src/api/routes/note.routes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const Controller = require('../controllers/Note.controller');
-const { db } = require('../../config/database');
 const Auth = require('../../lib/Auth');
 const { createValidator } = require('express-joi-validation');
 const { createNote } = require('../validators/Note.validators');
-const { wrap } = require('../../helpers/wrap');
+const wrap = require('../../helpers/wrap');
+const model = require('../models/Note.model');
 
 const validator = createValidator();
 
 const router = express.Router();
-const controller = new Controller({
-  model: db.note,
-});
+const controller = new Controller(model);
 
 router.post(
   '/create',
