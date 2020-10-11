c2323e519eb903b16fde8f559860fab406df83b9
Donny Roufs <iamchets@yahoo.com>
2020-10-11 12:13:07 +0200

    wrap handlers

--- server/src/api/routes/champion.routes.js
+++ server/src/api/routes/champion.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Controller = require('../controllers/Controller');
 const model = require('../models/Champion.model');
+const { db } = require('../../config/database');
+const wrap = require('../../helpers/wrap');
 
 const router = express.Router();
-const controller = new Controller(model);
+const controller = new Controller(model, db.champion);
 
-router.get('/', controller.all);
-router.get('/:name', controller.findOneByName);
+router.get('/', wrap(controller.all));
+router.get('/:name', wrap(controller.findOneByName));
 
 module.exports = router;
