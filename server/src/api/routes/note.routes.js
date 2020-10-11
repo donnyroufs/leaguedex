74b6fcf52d66f9d6398f68d7c2132551b814e769
Donny Roufs <iamchets@yahoo.com>
2020-10-11 12:45:05 +0200

    wrap handlers

--- server/src/api/routes/note.routes.js
+++ server/src/api/routes/note.routes.js
@@ -4,6 +4,7 @@ const { db } = require('../../config/database');
 const Auth = require('../../lib/Auth');
 const { createValidator } = require('express-joi-validation');
 const { createNote } = require('../validators/Note.validators');
+const { wrap } = require('../../helpers/wrap');
 
 const validator = createValidator();
 
@@ -16,9 +17,13 @@ router.post(
   '/create',
   Auth.authenticateToken,
   validator.body(createNote),
-  controller.createOne
+  wrap(controller.createOne)
 );
-router.delete('/:noteId', Auth.authenticateToken, controller.deleteOne);
-router.get('/dex/:id', Auth.authenticateToken, controller.findByMatchupId);
+router.delete('/:noteId', Auth.authenticateToken, wrap(controller.deleteOne));
+router.get(
+  '/dex/:id',
+  Auth.authenticateToken,
+  wrap(controller.findByMatchupId)
+);
 
 module.exports = router;
