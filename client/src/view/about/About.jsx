c97a4b7b5ae528f71867ba355efb0489c957a7b3
Donny Roufs <iamchets@yahoo.com>
2020-10-14 07:29:32 +0200

    very basic simple about page, still placeholder video

--- client/src/view/about/About.jsx
+++ client/src/view/about/About.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { Container, ImageWrapper } from "./About.styles";
+import {
+  Container,
+  ImageWrapper,
+  Article,
+  Background,
+  GithubLink,
+} from "./About.styles";
 import ReactPlayer from "react-player";
+import { FaGithub } from "react-icons/fa";
+
+const BreakLine = () => (
+  <>
+    <br />
+    <br />
+  </>
+);
 
 const About = (props) => {
   return (
     <Container>
+      <Background />
       <ImageWrapper>
         <ReactPlayer
           url="https://www.youtube.com/watch?v=XoYu7K6Ywkg"
@@ -13,6 +28,31 @@ const About = (props) => {
           height="100%"
         />
       </ImageWrapper>
+      <Article>
+        <Article.Title>What is Leaguedex?</Article.Title>
+        <Article.Body>
+          Then Leaguedex is for you. Keep track of all your played matchups in a
+          digital environment where you can write down your own personalized
+          notes to never ever forget again!
+          <BreakLine />
+          Perhaps people frequently ask you about how to play a certain champion
+          and you have been planning to write an in depth guide on how to play
+          but never really took the time to do so, since it costs a lot of free
+          time.. Well don’t worry!
+          <BreakLine />
+          Leaguedex has this covered for you. You can share your dex with others
+          or keep it to yourself. Whatever suits you.
+        </Article.Body>
+        <Article.Footer>
+          <GithubLink
+            href="https://github.com/donnyroufs/leaguedex"
+            target="_blank"
+          >
+            <FaGithub size="32" />
+            Github
+          </GithubLink>
+        </Article.Footer>
+      </Article>
     </Container>
   );
 };
--- client/src/view/about/About.styles.js
+++ client/src/view/about/About.styles.js
@@ -20,3 +20,46 @@ export const ImageWrapper = styled.div`
     height: 500px !important;
   }
 `;
+
+export const Article = styled.article``;
+
+Article.Title = styled.h1`
+  font-weight: 1000;
+  color: ${(props) => props.theme.primary};
+  margin-top: 6rem;
+  margin-bottom: 2rem;
+`;
+
+Article.Body = styled.p`
+  line-height: 2.1;
+  opacity: 0.75;
+
+  @media screen and (min-width: 968px) {
+    font-size: 1.1rem;
+  }
+`;
+Article.Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 3rem;
+`;
+
+export const Background = styled.div`
+  background-color: #18222f;
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  top: 30vh;
+  height: 90vh;
+  width: 100%;
+`;
+
+export const GithubLink = styled.a`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${(props) => props.theme.secondary};
+  text-decoration: none;
+  width: 100px;
+`;
