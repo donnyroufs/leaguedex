f972eb4d67fc52619ad65c9613a051b77474ee04
Donny Roufs <iamchets@yahoo.com>
2020-10-14 06:14:43 +0200

    add formatters

--- server/src/api/routes/shared.routes.js
+++ server/src/api/routes/shared.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const Controller = require('../controllers/Shared.controller');
 const model = require('../models/Shared.model');
 const wrap = require('../../helpers/wrap');
+const formatters = require('../formatters/shared.formatters');
 
 const router = express.Router();
-const controller = new Controller(model);
+const controller = new Controller(model, formatters);
 
 router.get('/:userId/dex/note', wrap(controller.findByMatchupId));
 router.get('/:username/dex', wrap(controller.findByUsernameAndId));
