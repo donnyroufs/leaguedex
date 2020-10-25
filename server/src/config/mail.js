8de2859bec8155ca438a77ebb943326dda7725fa
Donny Roufs <iamchets@yahoo.com>
2020-10-25 22:27:47 +0100

    sendEmail function

---
+++ server/src/config/mail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const inProduction = process.env.NODE_ENV === 'production';
+
+async function sendEmail(to, subject, mjmlTemplate) {
+  const message = {
+    to: to,
+    from: inProduction
+      ? process.env.SENDGRID_EMAIL
+      : 'development@leaguedex.com',
+    subject,
+    html: mjmlTemplate,
+  };
+
+  if (inProduction) {
+    await sgMail.send(message);
+  } else {
+    const testAccount = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+    const info = await transporter.sendMail(message);
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  }
+}
+
+module.exports = { sendEmail };
