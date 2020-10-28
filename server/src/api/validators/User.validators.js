15dd491526eb37fd8296eaf24bbd6fc894afaa37
Donny Roufs <iamchets@yahoo.com>
2020-10-28 16:00:20 +0100

    validation for resetting password

--- server/src/api/validators/User.validators.js
+++ server/src/api/validators/User.validators.js
@@ -11,4 +11,9 @@ module.exports = {
     username: Joi.string().min(4).max(16).required(),
     password: Joi.string().min(6).max(256).required(),
   }),
+  resetPassword: Joi.object({
+    token: Joi.string().required(),
+    password: Joi.string().min(6).max(256).required(),
+    password_confirmation: Joi.ref('password'),
+  }),
 };
