1c1b1edac5ba3685796db02cc41b3c6b1a3b8113
Donny Roufs <iamchets@yahoo.com>
2020-11-14 15:11:56 +0100

    get information about me

---
+++ client/src/hooks/useMeQuery.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { API } from "../api";
+
+export function useMeQuery() {
+  const [me, setMe] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    API.fetchMe()
+      .then((res) => res.json())
+      .then((data) => setMe(data))
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return {
+    me,
+    error,
+    loading,
+  };
+}
