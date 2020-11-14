304cf177ea2decb3c3357fce72a85ff1a20146f9
Donny Roufs <iamchets@yahoo.com>
2020-11-14 19:12:43 +0100

    mobile styling

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import * as SC from "./Settings.styles";
+import { Button } from "../../GlobalStyles";
+import Dropdown, { Menu } from "../../components/dropdown/Dropdown";
+import { useDropdown } from "../../hooks/useDropdown";
+
 
 const Settings = ({ me }) => {
+  const { show, handleSetShow } = useDropdown();
+
+  const handleDelete = (e) => {
+    console.log("delete");
+  };
+
+  const removeNumbersFromString = (str) => str.replace(/[0-9]/g, "");
+  const pluralize = (length = 1, str) => (length > 1 ? str + "s" : str);
+
   return (
     <SC.Container>
       <SC.Container.Inner>
-        <p>Settings page</p>
+        <SC.Box>
+          <SC.Title>Personal Information</SC.Title>
+          <SC.Box className="group">
+            <SC.InputGroup>
+              <SC.Label htmlFor="username">Username</SC.Label>
+              <SC.Input name="username" value={me.username} disabled />
+            </SC.InputGroup>
+            <SC.InputGroup>
+              <SC.Label htmlFor="email">Email Address</SC.Label>
+              <SC.Input name="email" value={me.email} disabled />
+            </SC.InputGroup>
+          </SC.Box>
+        </SC.Box>
+        <SC.Box className="password">
+          <SC.Title className="small">Password</SC.Title>
+          <Button>Change Password</Button>
+        </SC.Box>
+        <SC.Box className="summoner" as="ul">
+          <SC.Title className="summoner">
+            Summoner {pluralize(1, "Account")}
+          </SC.Title>
+          <SC.Field>
+            <SC.TextWrapper>
+              [{removeNumbersFromString(me.summoner?.region)}]
+            </SC.TextWrapper>
+            <SC.TextWrapper>{me.summoner?.name}</SC.TextWrapper>
+            <Dropdown show={show} handleSetShow={handleSetShow} right="12px">
+              <Menu.Item small onClick={handleDelete}>
+                delete
+              </Menu.Item>
+            </Dropdown>
+          </SC.Field>
+        </SC.Box>
       </SC.Container.Inner>
     </SC.Container>
   );
--- client/src/pages/settings/Settings.styles.js
+++ client/src/pages/settings/Settings.styles.js
@@ -4,9 +4,9 @@ export const Container = styled.section`
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
+  margin-bottom: 8rem;
 
   @media screen and (min-width: 968px) {
-    margin-top: -100px;
     height: calc(100vh - 175px);
   }
 `;
@@ -14,4 +14,70 @@ export const Container = styled.section`
 Container.Inner = styled.div`
   width: 80%;
   margin: 0 auto;
+`;
+
+export const Box = styled.div`
+  &.group {
+    margin: 3rem 0 2rem 0;
+  }
+
+  &.password {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3rem 0;
+    border-top: 1px solid #263547;
+    border-bottom: 1px solid #263547;
+  }
+
+  &.summoner {
+    margin: 0;
+    padding: 3rem 0;
+    list-style: none;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 1.1rem;
+  margin: 0;
+
+  &.small {
+    font-size: 0.95rem;
+  }
+
+  &.summoner {
+    margin-bottom: 3rem;
+  }
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+export const Label = styled.label`
+  margin: 0 0 1rem 0;
+`;
+
+export const Input = styled.input`
+  padding: 1.25rem 1.5rem;
+  margin-bottom: 2.5rem;
+  outline: none;
+  border: none;
+  border-radius: 0.3em;
+  color: ${({ theme }) => theme.placeholderText};
+  background: ${({ theme }) => theme.header};
+`;
+
+export const Field = styled.li`
+  position: relative;
+  padding: 1.25rem 1.5rem;
+  border-radius: 0.3rem;
+  color: ${({ theme }) => theme.placeholderText};
+  background: ${({ theme }) => theme.header};
+  margin: 0 0 1.25rem 0;
+`;
+
+export const TextWrapper = styled.span`
+  margin-right: 0.5rem;
 `;
