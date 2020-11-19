a7623125b1b17a7b1e25dd5532a4aba7ce939ed3
Donny Roufs <iamchets@yahoo.com>
2020-11-19 13:10:11 +0100

    add blacklist email helper

--- server/src/helpers/utils.js
+++ server/src/helpers/utils.js
@@ -5,4 +5,9 @@ function normalize(championName) {
     .trim();
 }
 
-module.exports = { normalize };
+function blackListEmail(email) {
+  const regex = /[a-zA-Z0-9_\.+]+@(live|hotmail|outlook)/;
+  return !!email.match(regex);
+}
+
+module.exports = { normalize, blackListEmail };
