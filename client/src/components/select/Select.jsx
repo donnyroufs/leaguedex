54de7bdb72394eb763a698113e45cf4036da8f96
Donny Roufs <iamchets@yahoo.com>
2020-11-21 23:58:13 +0100

    styles

--- client/src/components/select/Select.jsx
+++ client/src/components/select/Select.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import * as SC from "./Select.styles";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 const Select = ({ value, onChange, options }) => {
+  const [open, setOpen] = useState(false);
   return (
-    <SC.Select onChange={onChange} value={value}>
-      {options.map((option) => (
-        <SC.Option value={option.value} key={option.value}>
-          {option.label}
-        </SC.Option>
-      ))}
-    </SC.Select>
+    <SC.Wrapper onClick={() => setOpen(curr => !curr)}>
+      <SC.Select onChange={onChange} value={value}>
+        {options.map((option) => (
+          <SC.Option value={option.value} key={option.value}>
+            {option.label}
+          </SC.Option>
+        ))}
+      </SC.Select>
+      <SC.ArrowWrapper>
+        {!open && <FaChevronDown />}
+        {open && <FaChevronUp />}
+      </SC.ArrowWrapper>
+    </SC.Wrapper>
   );
 };
 
