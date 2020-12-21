aaf20e264ce4b472e93531514072cb44ced5f13f
Donny Roufs <iamchets@yahoo.com>
2020-12-21 16:33:08 +0100

    add dropdown

--- client/src/components/notification/Notification.jsx
+++ client/src/components/notification/Notification.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "../../GlobalStyles";
 import * as SC from "./Notification.styles";
+import { ReactComponent as MarkConfirm } from "./yes-icon.svg";
+import { ReactComponent as MarkCancel } from "./no-icon.svg";
 
 const Notification = () => {
+  const [show, setShow] = useState(true);
+
   const count = 2;
+
   return (
     <SC.Container>
-      <SC.IconWrapper>
+      <SC.IconWrapper onClick={() => setShow((curr) => !curr)}>
         <SC.Icon />
         {count > 0 && <SC.Bubble>{count}</SC.Bubble>}
       </SC.IconWrapper>
+      <SC.Wrapper show={show}>
+        <SC.Heading>Unconfirmed matches</SC.Heading>
+        <SC.Dropdown>
+          <SC.Item>
+            <SC.Info>
+              <SC.Lane>T</SC.Lane>
+              <SC.Result win={true}>W</SC.Result>
+              <SC.Details>Akali vs Renekton</SC.Details>
+            </SC.Info>
+            <SC.ConfirmWrapper>
+              {/* <MarkConfirm />
+              <MarkCancel /> */}
+            </SC.ConfirmWrapper>
+          </SC.Item>
+        </SC.Dropdown>
+        <SC.Footer>
+          <Button logout style={{ width: "100%", marginLeft: "0" }}>
+            Confirm
+          </Button>
+        </SC.Footer>
+      </SC.Wrapper>
     </SC.Container>
   );
 };
--- client/src/components/notification/Notification.styles.js
+++ client/src/components/notification/Notification.styles.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 import { FaBell } from "react-icons/fa";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const Wrapper = styled.div`
+  display: ${(props) => (props.show ? "flex" : "none")};
+  flex-flow: column nowrap;
+  position: absolute;
+  border: 1px solid #263547;
+  border-radius: 6px;
+  box-shadow: 0px 4px 8px -4px rgba(18, 31, 46, 0.62);
+  background: #18222f;
+  z-index: 10;
+  right: 2px;
+  margin-top: 1rem;
+  width: 450px;
+  padding: 2rem;
+`;
 
 export const IconWrapper = styled.div`
   position: relative;
@@ -10,6 +27,7 @@ export const IconWrapper = styled.div`
   border-radius: 6px;
   margin-left: 1.25rem;
   display: flex;
+  flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
   height: 100%;
@@ -40,3 +58,48 @@ export const Bubble = styled.div`
 export const Icon = styled(FaBell)`
   font-size: 1.3rem;
 `;
+
+export const Dropdown = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+export const Heading = styled.h1`
+  font-size: 0.9rem;
+  opacity: 0.8;
+  border-bottom: 1px solid #263547;
+  padding-bottom: 1.5rem;
+  letter-spacing: 0.1em;
+  font-weight: 500;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+  transition: all 0.1s ease-in;
+  width: 100%;
+`;
+
+export const Info = styled.div``;
+export const Details = styled.span``;
+
+export const Lane = styled.span`
+  font-weight: bold;
+  margin-right: 0.3rem;
+`;
+
+export const Result = styled.span`
+  font-weight: bold;
+  margin-right: 0.6rem;
+  color: ${(props) => (props.win ? "#4ED864" : "#BF545B")};
+`;
+
+export const Footer = styled.footer`
+  padding: 0;
+  margin-top: 4rem;
+`;
+
+export const ConfirmWrapper = styled.div``;
