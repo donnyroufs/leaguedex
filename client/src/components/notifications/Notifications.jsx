808e184414c90ee93f7e3a86875bf1611670e5e3
Donny Roufs <iamchets@yahoo.com>
2020-12-22 21:00:52 +0100

    dont show on initial load

--- client/src/components/notifications/Notifications.jsx
+++ client/src/components/notifications/Notifications.jsx
@@ -6,7 +6,7 @@ import Notification from "./components/notification/Notification";
 import { useNotifications } from "../../hooks/useNotifications";
 
 const Notifications = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   const [choices, setChoices] = useState({});
   const { notifications } = useNotifications();
 
