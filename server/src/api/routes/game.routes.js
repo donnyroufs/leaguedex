506255c2aab429ff9b86899451356934aade77db
Donny Roufs <iamchets@yahoo.com>
2021-01-15 22:24:35 +0100

    implements game route to get current notifications of last played games

--- server/src/api/routes/game.routes.js
+++ server/src/api/routes/game.routes.js
@@ -3,9 +3,10 @@ const Controller = require('../controllers/Game.controller');
 const Auth = require('../../lib/Auth');
 const wrap = require('../../helpers/wrap');
 const model = require('../models/Game.model');
+const formatter = require('../formatters/game.formatters');
 
 const router = express.Router();
-const controller = new Controller(model);
+const controller = new Controller(model, formatter);
 
 router.use(Auth.authenticateToken);
 
