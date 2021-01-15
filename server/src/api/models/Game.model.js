5e3f5f7c0726dd9600ba713d8431c4fe388e042f
Donny Roufs <iamchets@yahoo.com>
2021-01-15 22:25:13 +0100

    init and nearly finished feature for match history notifications / confirming

---
+++ server/src/api/models/Game.model.js
@@ -0,0 +1,101 @@
+const { db } = require('../../config/database');
+const Model = require('./Model');
+const lodash = require('lodash');
+
+class GameModel extends Model {
+  constructor(...props) {
+    super(...props);
+  }
+
+  async getRecentGameDate(userId) {
+    const resources = await db.game.findMany({
+      where: {
+        user_id: userId,
+      },
+      select: {
+        timestamp: true,
+      },
+    });
+
+    if (!resources) return [];
+
+    const [lastPlayedGameFirst] = resources.sort(
+      (a, b) => Number(b.timestamp) - Number(a.timestamp)
+    );
+
+    return lastPlayedGameFirst;
+  }
+
+  async getSummonerInfo(summonerId) {
+    const resource = await db.summoner.findOne({
+      where: {
+        id: summonerId,
+      },
+    });
+
+    return resource;
+  }
+
+  async getChampionById(id) {
+    const resource = await db.champion.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return resource;
+  }
+
+  async saveAndGetNotifications(matchHistory, userId, summonerId) {
+    const currentMatchHistory = await db.game.findMany({
+      where: {
+        user_id: Number(userId),
+        summoner_id: String(summonerId),
+      },
+      select: {
+        game_id: true,
+      },
+    });
+
+    const uniqueMatchHistory = matchHistory.filter((m) => {
+      const mapped = currentMatchHistory.map((match) => match.game_id);
+      return !mapped.includes(String(m.gameId));
+    });
+
+    const queries = uniqueMatchHistory.map((match) => {
+      return this.db.upsert({
+        where: {
+          user_id_game_id_summoner_id: {
+            game_id: String(match.gameId),
+            user_id: Number(userId),
+            summoner_id: String(summonerId),
+          },
+        },
+        create: {
+          game_id: String(match.gameId),
+          user_id: Number(userId),
+          region: match.platformId,
+          status: 'pending',
+          type: 'notification',
+          timestamp: String(match.timestamp),
+          lane: match.lane,
+          summoner_id: String(summonerId),
+        },
+        update: {},
+      });
+    });
+
+    await Promise.all(queries);
+
+    return this.db.findMany({
+      where: {
+        user_id: Number(userId),
+        summoner_id: String(summonerId),
+        type: 'notification',
+        status: 'pending',
+      },
+    });
+  }
+}
+
+module.exports = new GameModel(db.game);
