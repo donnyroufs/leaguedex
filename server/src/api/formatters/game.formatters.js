31816ee477f1ccd719a2827f227db330cc37a04a
Donny Roufs <iamchets@yahoo.com>
2021-01-18 15:45:11 +0100

    add timestamp to game and sort

--- server/src/api/formatters/game.formatters.js
+++ server/src/api/formatters/game.formatters.js
@@ -1,23 +1,26 @@
 module.exports = {
   initialNotifications: (games) => {
-    return games.map(({ id, me, championA, opponent, championB, win }) => ({
-      id,
-      me: {
-        win,
-        championA: {
-          id: championA.id,
-          name: championA.name,
+    return games
+      .map(({ id, me, timestamp, championA, opponent, championB, win }) => ({
+        id,
+        timestamp,
+        me: {
+          win,
+          championA: {
+            id: championA.id,
+            name: championA.name,
+          },
+          lane: me.timeline.lane,
         },
-        lane: me.timeline.lane,
-      },
-      opponent: {
-        win: !win,
-        championB: {
-          id: championB.id,
-          name: championB.name,
+        opponent: {
+          win: !win,
+          championB: {
+            id: championB.id,
+            name: championB.name,
+          },
+          lane: opponent.timeline.lane,
         },
-        lane: opponent.timeline.lane,
-      },
-    }));
+      }))
+      .sort((a, b) => b.timestamp - a.timestamp);
   },
 };
