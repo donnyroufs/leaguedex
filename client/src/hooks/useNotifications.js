a0b407ecfbc9deed34ba3563e5d1229674ee2998
Donny Roufs <iamchets@yahoo.com>
2021-01-19 16:53:44 +0100

    update notifications and add loading state

--- client/src/hooks/useNotifications.js
+++ client/src/hooks/useNotifications.js
@@ -22,32 +22,19 @@ export const useNotifications = () => {
 };
 
 const useNotificationsProvider = () => {
-  const { activeSummonerId } = useMatch();
+  const [loading, setLoading] = useState(false);
+  const { activeSummonerId, afterUpdateNotifications } = useMatch();
   const { user } = useAuth();
 
   const [notifications, setNotifications] = useState([]);
 
-  // Get the last registered date
-  // TODO: Fix bug where it doesn't find the ID upon creating a new account.
-  // useEffect(() => {
-  //   if (!activeSummonerId || !user) return;
-  //   if (user.summoner.length <= 0) return;
-  //
-  //   const currentSummoner = user.summoner.find(
-  //     (s) => s.accountId === activeSummonerId
-  //   );
-
-  //   API.playground(currentSummoner.id);
-  // }, [activeSummonerId, user]);
-
   useEffect(() => {
-    if (!user) return;
+    if (!user || !activeSummonerId) return;
 
     const currentSummoner = user.summoner.find(
       (s) => s.accountId === activeSummonerId
     );
 
-    // summonerId need to rename in prisma
     if (!currentSummoner) return;
 
     API.getMatchNotifications(currentSummoner.id, currentSummoner.summonerId)
@@ -55,7 +42,50 @@ const useNotificationsProvider = () => {
       .then((data) => setNotifications(data));
   }, [user]);
 
+  async function updateNotifications(payload) {
+    if (!activeSummonerId) {
+      return null;
+    }
+
+    const currentSummoner = user.summoner.find(
+      (s) => s.accountId === activeSummonerId
+    );
+
+    if (!currentSummoner) {
+      return null;
+    }
+
+    setLoading(true);
+
+    const response = await API.updateMatchNotifications(
+      payload,
+      currentSummoner.summonerId
+    ).catch((err) => console.log(err));
+
+    const isSuccess = response.status === 201;
+
+    if (isSuccess) {
+      const updatedNotifications = notifications.filter(
+        (n) => !Object.keys(payload).includes(n.id)
+      );
+      setNotifications(updatedNotifications);
+
+      // Get json data of current matchups and pass to afterUpdate
+      // await afterUpdateNotifications(data).catch(
+      //   (err) => {
+      //     console.log(err);
+      //   }
+      // );
+    }
+
+    setLoading(false);
+
+    return isSuccess;
+  }
+
   return {
     notifications,
+    updateNotifications,
+    loading,
   };
 };
