d30599eb462ccd8744bbb21b2cbf28f0b79ad4b6
Donny Roufs <iamchets@yahoo.com>
2021-01-25 14:32:31 +0100

    add loading state and remove obsolete stuff

--- client/src/hooks/useModal.js
+++ client/src/hooks/useModal.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const modalContext = createContext();
 
