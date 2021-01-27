aeb64340be622f10727b607aad1175ca1cf1b3c3
Donny Roufs <iamchets@yahoo.com>
2021-01-27 17:01:21 +0100

    add user if there is one

--- server/src/api/routes/shared.routes.js
+++ server/src/api/routes/shared.routes.js
@@ -3,9 +3,12 @@ const Controller = require('../controllers/Shared.controller');
 const model = require('../models/Shared.model');
 const wrap = require('../../helpers/wrap');
 const formatters = require('../formatters/shared.formatters');
+const Auth = require('../../lib/Auth');
 
 const router = express.Router();
 const controller = new Controller(model, formatters);
+
+router.use(Auth.withUser);
 
 router.get('/:userId/dex/note', wrap(controller.findByMatchupId));
 router.get('/:username/dex', wrap(controller.findByUsernameAndId));
