fc78e456c516c2319c3d86201d63dd322b16cc8a
Donny Roufs <iamchets@yahoo.com>
2021-01-27 15:08:43 +0100

    change color

--- client/src/components/info/Info.styles.js
+++ client/src/components/info/Info.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { AiFillInfoCircle } from "react-icons/ai";
 
 export const InfoIcon = styled(AiFillInfoCircle)`
-  color: ${({ theme }) => theme.primary};
+  color: #007aff;
   font-size: 1.55rem;
   margin: 0 0.3rem 0.1rem 0.3rem;
   transition: opacity 0.1s ease-in;
