0c98e4dc17f1a38fc6246c6de53dca2fd9fb4911
Donny Roufs <iamchets@yahoo.com>
2021-02-04 20:31:54 +0100

    refactor layout margin

--- client/src/components/layout/Layout.styles.js
+++ client/src/components/layout/Layout.styles.js
@@ -4,9 +4,4 @@ export const Container = styled.div`
   position: relative;
 `;
 
-export const Wrapper = styled.div`
-  margin-top: 75px;
-  @media screen and (min-width: 968px) {
-    margin-top: 275px;
-  }
-`;
+export const Wrapper = styled.div``;
